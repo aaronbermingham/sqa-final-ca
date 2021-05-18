d1f783d8d04d95144374971016a792f04dc4e03b
Aaron Bermingham <aaronbrmnghm@gmail.com>
2021-05-18 14:48:57 +0100

    Created criterion class

---
+++ src/classes/Criterion.java
@@ -0,0 +1,28 @@
+package classes;
+
+public class Criterion {
+	public String name;
+	public double grade;
+
+	public Criterion(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
+}
