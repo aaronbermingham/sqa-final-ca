16f2495bbe530e7e40d6ca436123a000b1d5e64a
Aaron Bermingham <aaronbrmnghm@gmail.com>
2021-05-19 15:16:21 +0100

    Formatted & corrected indentation

--- src/classes/Controller.java
+++ src/classes/Controller.java
@@ -5,26 +5,25 @@ public class Controller {
 		Rubric rubric = new Rubric(name);
 		return rubric;
 	}
-	
+
 	public Criterion createCriterion(String name) {
 		Criterion criterion = new Criterion(name);
 		return criterion;
 	}
-	
+
 	public void addCriteriontoRubric(Rubric r, Criterion c) {
 		// Check for the number of criterion associated with the rubric
 		int size = r.getCriteria().size();
 		System.out.println("Size before " + r.getCriteria().size());
 		// Check if the number of criterion are greater or equal to 10
-		if(size >= 10) {
+		if (size >= 10) {
 			// Throw exception if size is greater or equal to tne
-		        throw new IllegalStateException("Rubric should can only contain up to ten criterion");
-		      }
-		else {
-			// Other wise add the criterion to the rubric 
+			throw new IllegalStateException("Rubric should can only contain up to ten criterion");
+		} else {
+			// Other wise add the criterion to the rubric
 			r.getCriteria().add(c);
 			System.out.println("Size after " + r.getCriteria().size());
 		}
-		//return r;
+		// return r;
 	}
 }
--- src/test/RubricTests.java
+++ src/test/RubricTests.java
@@ -1,4 +1,5 @@
 package test;
+
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
@@ -9,53 +10,52 @@ import classes.Controller;
 import classes.Criterion;
 import classes.Rubric;
 
-
-
 public class RubricTests {
 	Controller cntrllr = new Controller();
 	ArrayList<Rubric> rubricList = new ArrayList<Rubric>();
 	ArrayList<Criterion> criterionList = new ArrayList<Criterion>();
 	String s = "Rubric title";
 	Rubric r = cntrllr.createRubric(s);
-	
+
 	@Test
 	public void testRubricCreation() {
-		
+
 		assertEquals(s, r.getTitle());
 	}
-	
+
 	// Test for adding to a rubric that already has ten criterion
-	 @Test(expected = IllegalStateException.class)
-	    public void testAddRubricCriterionFail() {
-		 	// Criterion objects
-	        Criterion c1 = new Criterion("TestCriterion");
-	        Criterion c2 = new Criterion("NewCriterion");
-	        // Add criterion object c1 so rubric has ten criteria
-	        for(int i = 0; i < 10; i ++) {
-	        	criterionList.add(c1);
-	        }
-	        // Associate the criterion list with a rubric
-	        r.setCriteria(criterionList);
-	        /* Attempt to add a new criterion to the rubric 
-	         * This should result in a exception being thrown
-	         * */
-	        cntrllr.addCriteriontoRubric(r, c2);   
-	    }
-	 
-	 @Test
-	 public void testAddRubricCriterionSuccess() {
+	@Test(expected = IllegalStateException.class)
+	public void testAddRubricCriterionFail() {
 		// Criterion objects
-	        Criterion c1 = new Criterion("TestCriterion");
-	        Criterion c2 = new Criterion("NewCriterion");
-	        // Add criterion object c1 so rubric has 6 criteria
-	        for(int i = 0; i < 6; i ++) {
-	        	criterionList.add(c1);
-	        }
-	        // Associate the criterion list with a rubric
-	        r.setCriteria(criterionList);
-	        // Add new
-	        cntrllr.addCriteriontoRubric(r, c2);   
-	        assertEquals(7, r.getCriteria().size());
-	    }
-	
+		Criterion c1 = new Criterion("TestCriterion");
+		Criterion c2 = new Criterion("NewCriterion");
+		// Add criterion object c1 so rubric has ten criteria
+		for (int i = 0; i < 10; i++) {
+			criterionList.add(c1);
+		}
+		// Associate the criterion list with a rubric
+		r.setCriteria(criterionList);
+		/*
+		 * Attempt to add a new criterion to the rubric This should result in a
+		 * exception being thrown
+		 */
+		cntrllr.addCriteriontoRubric(r, c2);
+	}
+
+	@Test
+	public void testAddRubricCriterionSuccess() {
+		// Criterion objects
+		Criterion c1 = new Criterion("TestCriterion");
+		Criterion c2 = new Criterion("NewCriterion");
+		// Add criterion object c1 so rubric has 6 criteria
+		for (int i = 0; i < 6; i++) {
+			criterionList.add(c1);
+		}
+		// Associate the criterion list with a rubric
+		r.setCriteria(criterionList);
+		// Add new
+		cntrllr.addCriteriontoRubric(r, c2);
+		assertEquals(7, r.getCriteria().size());
+	}
+
 }
